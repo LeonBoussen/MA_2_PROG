0d4d9658ad416e482cdf7d02d6db7f856ee925a0
Leon <jurassicparkleon@gmail.com>
2022-01-24 15:08:48 +0100

    first commit

---
+++ Les01/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les01
+{
+    class Database
+    {
+         
+    }
+}
---
+++ Les01/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les01
+{
+    public abstract class Item
+    {
+        public string Name { get; private set; }
+    
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Use();
+    }
+}
